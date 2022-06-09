db2bf6ff07c0ce36a2cb129c3f0f9dfc247f0e5e
Mohit <mohitsharma8864@gmail.com>
2022-06-10 00:33:23 +0530

    form basics

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,25 @@
 import React, { useRef } from 'react'
-import { Container, Form} from 'react-bootstrap'
+import { Container, Form, Button} from 'react-bootstrap'
+import { v4 as uuidV4 } from 'uuid';
 
-export default function Login() {
+export default function Login( {onIdSubmit} ) {
     const idRef = useRef()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        onIdSubmit(idRef.current.value)
+    }
 
     return (
         <Container className='align-items-center d-flex' style={{ height: '100v'}}>
-            <Form>
+            <Form onSubmit={handleSubmit} className='w-100'>
                 <Form.Group>
                     <Form.Label>Enter Your Id</Form.Label>
                     <Form.Control type="text" ref={idRef} required/>
                 </Form.Group>
+                <Button type="submit" className='mr-2'>Login</Button>
+                <Button variant="secondary">Create a New Id</Button>
             </Form>
         </Container>
   )
